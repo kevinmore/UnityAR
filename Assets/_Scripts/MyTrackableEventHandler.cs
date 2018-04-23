e32f123b93d87839c2cd4ef5ba9214c1fd4b9779
Kevin Wang <kevin@zappar.com>
2018-04-23 15:35:09 +0100

    Added a customized tracking event handler script to reposition the cube when found/lost the image target.

---
+++ Assets/_Scripts/MyTrackableEventHandler.cs
@@ -0,0 +1,91 @@
+﻿/*==============================================================================
+Copyright (c) 2017 PTC Inc. All Rights Reserved.
+
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+///     A custom handler that implements the ITrackableEventHandler interface.
+/// </summary>
+public class MyTrackableEventHandler : MonoBehaviour, ITrackableEventHandler
+{
+    protected TrackableBehaviour mTrackableBehaviour;
+    private Vector3 originalPos;
+
+    public Transform anchorPoint;
+    public GameObject ARObj;
+    public float staticDistanceToScreen;
+
+    #region UNITY_MONOBEHAVIOUR_METHODS
+
+    protected virtual void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (mTrackableBehaviour)
+            mTrackableBehaviour.UnregisterTrackableEventHandler(this);
+    }
+
+    #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    #region PUBLIC_METHODS
+
+    /// <summary>
+    ///     Implementation of the ITrackableEventHandler function called when the
+    ///     tracking state changes.
+    /// </summary>
+    public void OnTrackableStateChanged(
+        TrackableBehaviour.Status previousStatus,
+        TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " found");
+            OnTrackingFound();
+        }
+        else if (previousStatus == TrackableBehaviour.Status.TRACKED &&
+                 newStatus == TrackableBehaviour.Status.NOT_FOUND)
+        {
+            Debug.Log("Trackable " + mTrackableBehaviour.TrackableName + " lost");
+            OnTrackingLost();
+        }
+        else
+        {
+            // For combo of previousStatus=UNKNOWN + newStatus=UNKNOWN|NOT_FOUND
+            // Vuforia is starting, but tracking has not been lost or found yet
+            // Call OnTrackingLost() to hide the augmentations
+            OnTrackingLost();
+        }
+    }
+
+    #endregion // PUBLIC_METHODS
+
+    #region PROTECTED_METHODS
+
+    protected virtual void OnTrackingFound()
+    {
+        // set the obj back to its original pos
+        ARObj.transform.position = anchorPoint.position;
+    }
+
+    protected virtual void OnTrackingLost()
+    {
+        // set the obj to the center of screen
+        ARObj.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2.0f,
+            Screen.height / 2.0f, Camera.main.nearClipPlane + staticDistanceToScreen));
+    }
+
+    #endregion // PROTECTED_METHODS
+}
