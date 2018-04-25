06e68095e3ed1da99c26ac61ac447c1ecd921744
Kevin Wang <kevin@zappar.com>
2018-04-25 12:55:14 +0100

    Added a simple script to rotate the cube.

---
+++ Assets/_Scripts/CubeRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeRotator : MonoBehaviour
+{
+
+	public float degreesPerSecond = 30.0f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * Time.deltaTime * degreesPerSecond, Space.World);
+	}
+}
